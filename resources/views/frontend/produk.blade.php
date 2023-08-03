13d1ae45a4e966d911f868e5d028c5706b0c2c10
ardi12345678978 <benedictusardi1@gmail.com>
2023-08-03 21:04:06 +0700

    Update produk.blade.php

--- resources/views/frontend/produk.blade.php
+++ resources/views/frontend/produk.blade.php
@@ -77,6 +77,27 @@
             <h5 class="card-title">Judul Produk 3</h5>
           </div>
           <!-- Remove Product actions-->
+          </div>
+      </div>
+      <div class="col mb-1">
+        <div class="card h-100">
+          <!-- Sale badge-->
+          <div class="badge badge-custom bg-success text-white position-absolute" style="top: 0; right: 0">
+            Tersedia
+          </div>
+          <!-- Product image-->
+          <a href="{{ route('detail') }}">
+            <img
+                class="card-img-top"
+                src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+                alt="..."
+              />
+             </a>
+          <!-- Product title -->
+          <div class="card-body">
+            <h5 class="card-title">Judul Produk 4</h5>
+          </div>
+          <!-- Remove Product actions-->
         </div>
       </div>
     </div>
@@ -148,80 +169,32 @@
             <h5 class="card-title">Judul Produk 6</h5>
           </div>
           <!-- Remove Product actions-->
+          </div>
+      </div>
+      <div class="col mb-1">
+        <div class="card h-100">
+          <!-- Sale badge-->
+          <div class="badge badge-custom bg-success text-white position-absolute" style="top: 0; right: 0">
+            Tersedia
+          </div>
+          <!-- Product image-->
+          <a href="{{ route('detail') }}">
+            <img
+                class="card-img-top"
+                src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
+                alt="..."
+              />
+             </a>
+          <!-- Product title -->
+          <div class="card-body">
+            <h5 class="card-title">Judul Produk 7</h5>
+          </div>
+          <!-- Remove Product actions-->
         </div>
       </div>
     </div>
   </div>
 </section>
+
 
-<!-- Section-->
-<section class="py-5">
-  <div class="container px-4 px-lg-5 mt-5">
-    <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-      <div class="col mb-1">
-        <div class="card h-100">
-          <!-- Sale badge-->
-          <div class="badge badge-custom bg-warning text-white position-absolute" style="top: 0; right: 0">
-            Tidak Tersedia
-          </div>
-          <!-- Product image-->
-          <a href="{{ route('detail') }}">
-            <img
-                class="card-img-top"
-                src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
-                alt="..."
-              />
-             </a>
-          <!-- Product title -->
-          <div class="card-body">
-            <h5 class="card-title">Judul Produk 4</h5>
-          </div>
-          <!-- Remove Product actions-->
-        </div>
-      </div>
-      <div class="col mb-1">
-        <div class="card h-100">
-          <!-- Sale badge-->
-          <div class="badge badge-custom bg-success text-white position-absolute" style="top: 0; right: 0">
-            Tersedia
-          </div>
-          <!-- Product image-->
-          <a href="{{ route('detail') }}">
-            <img
-                class="card-img-top"
-                src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
-                alt="..."
-              />
-             </a>
-          <!-- Product title -->
-          <div class="card-body">
-            <h5 class="card-title">Judul Produk 5</h5>
-          </div>
-          <!-- Remove Product actions-->
-        </div>
-      </div>
-      <div class="col mb-1">
-        <div class="card h-100">
-          <!-- Sale badge-->
-          <div class="badge badge-custom bg-success text-white position-absolute" style="top: 0; right: 0">
-            Tersedia
-          </div>
-          <!-- Product image-->
-          <a href="{{ route('detail') }}">
-            <img
-                class="card-img-top"
-                src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg"
-                alt="..."
-              />
-             </a>
-          <!-- Product title -->
-          <div class="card-body">
-            <h5 class="card-title">Judul Produk 6</h5>
-          </div>
-          <!-- Remove Product actions-->
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
 @endsection
